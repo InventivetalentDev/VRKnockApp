65516b29f620ba9e5e22a3cf0e36a8c03e4543c7
inventivetalent <mail@inventivetalent.org>
2020-01-03 20:32:57 +0100

    change to multidex

---
+++ app/src/main/java/org/inventivetalent/vrknock/VRKnockApplication.java
@@ -0,0 +1,10 @@
+package org.inventivetalent.vrknock;
+
+import androidx.multidex.MultiDexApplication;
+
+public class VRKnockApplication extends MultiDexApplication {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
